4b134c7b5f747606923a47ef4dbf7a61fa6d1b1f
Lee Prany <jenny.prany@gmail.com>
2023-09-25 15:14:52 -0500

    first push

---
+++ src/main/java/tests/BasicTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import io.qameta.allure.Feature;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/*
+ * Basic tests
+ */
+public class BasicTests {
+    @Test
+    @Feature("Pass")
+    public void alwaysPass() {
+        boolean alwaysTrue = true;
+        System.out.println("Condition is: " + alwaysTrue);
+        Assert.assertTrue(alwaysTrue);
+    }
+
+    @Test
+    @Feature("Fail")
+    public void alwaysFail() {
+        boolean alwaysFalse = false;
+        System.out.println("Condition is: " + alwaysFalse);
+        Assert.assertTrue(alwaysFalse);
+    }
+}
