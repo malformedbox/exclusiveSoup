00d28456c49dcaab91590dc2c8c3d6b4ff2c2c04
malformedbox <74320514+malformedbox@users.noreply.github.com>
2024-01-10 15:43:45 -0600

    Update BasicPages.java

--- src/java/pages/BasicPages.java
+++ src/java/pages/BasicPages.java
@@ -5,6 +5,6 @@ import io.qameta.allure.Step;
 public class BasicPages {
     @Step("Print {message} message")
     public void printOutAssert(String message) {
-        System.out.println(message);
+        System.out.println("Build number: " + System.getProperty("buildNumber") + " " + message);
     }
 }
