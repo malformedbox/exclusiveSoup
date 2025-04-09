570b9acf0d417e529a037b4c0bdc97e7da2ed10f
Lee Prany <jenny.prany@gmail.com>
2025-04-09 18:23:50 -0500

    updates

--- src/java/tests/delete/SimpleMath.java
+++ src/java/tests/delete/SimpleMath.java
@@ -1,18 +1,35 @@
 package tests.delete;
 
 import io.qameta.allure.Feature;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.BasicPages;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /*
  * Math tests
  */
 public class SimpleMath {
+    public WebDriver driver;
     BasicPages basicPages = new BasicPages();
     @Test
     @Feature("Addition")
     public void addition() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setCapability("browserVersion", "LATEST");
+        System.out.println(System.getProperty("bsUsername"));
+        System.out.println(System.getProperty("bsAccessKey"));
+        try {
+            WebDriver driver = new RemoteWebDriver(new URL("https://" + System.getProperty("bsUsername") + ":" + System.getProperty("bsAccessKey") + "@hub-cloud.browserstack.com/wd/hub"), chromeOptions);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
         int sum = 4;
         basicPages.printOutAssert("Sum is: " + sum);
         Assert.assertEquals(2, 2, sum);
